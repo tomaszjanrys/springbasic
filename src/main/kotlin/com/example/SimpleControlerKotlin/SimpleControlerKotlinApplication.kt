6f2b797264209a774036fb31b25ec398edf0b93c
nice <tomaszjanrys@gmail.com>
2020-10-12 16:33:24 +0200

    cd 1 .....web

---
+++ src/main/kotlin/com/example/SimpleControlerKotlin/SimpleControlerKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.example.SimpleControlerKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+//zdefiniowana pusta klasa
+class SimpleControlerKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleControlerKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/SimpleControlerKotlin/SimpleControlerKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SimpleControlerKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleControlerKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
